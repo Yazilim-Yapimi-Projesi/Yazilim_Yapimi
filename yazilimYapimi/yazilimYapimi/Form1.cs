bf1d2e1bf3851cf7682dbbfd7aa7d36509c914e1
Hkaya50126 <58502933+Hkaya50126@users.noreply.github.com>
2021-05-15 16:53:17 +0300

    Vize_Hamza
    - Giriş yap, Üye Ol ekranları ve işlemleri 
    -  Ana Sayfa ekranının geliştirilmesi 
    - Profil sayfasının tasarımı ve işlemleri
    - Para Yatır sayfasının tasarımı
    - Ürün Sat sayfasının tasarımı ve işlemleri
    - Alım Yap sayfasının tasarımı ve İşlemlerin belli kısımları yapılmıştır.

---
+++ yazilimYapimi/yazilimYapimi/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace yazilimYapimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti;
+        OleDbCommand komut;
+        OleDbDataReader oku;
+        private void Temizle()
+        {
+            txtTCLogin.Clear();
+            txtSifreLogin.Clear();
+        }
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            if (txtTCLogin.Text==""||txtSifreLogin.Text=="")
+            {
+                MessageBox.Show("Lütfen İstenilen Alanları Doldurunuz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Borsa.accdb");
+                baglanti.Open();
+
+                komut = new OleDbCommand();
+                komut.CommandText = "select * from Users where User_TC_Number=@tc and Password=@sifre and UserTypeName=@usertypename";
+                komut.Connection = baglanti;
+                int userid=0;
+
+                komut.Parameters.AddWithValue("@tc", txtTCLogin.Text);
+                komut.Parameters.AddWithValue("@sifre", txtSifreLogin.Text);
+
+                if (rbtnAdmin.Checked == true)
+                {
+                    komut.Parameters.AddWithValue("@usertypename", "Admin");
+                }
+                else
+                {
+                    komut.Parameters.AddWithValue("@usertypename", "Kullanıcı");
+                }
+
+                
+                oku = komut.ExecuteReader();
+                if (oku.Read())
+                {
+                    if (oku[1].ToString()=="Admin")
+                    {
+                        Form3 f3 = new Form3();
+                        f3.tcProfil.Text = txtTCLogin.Text;
+                        Form2 f2 = new Form2();
+                        this.Hide();
+                        f2.Show();
+                    }
+                    else
+                    {
+                        userid = Convert.ToInt32(oku[0]);
+                        Form3 f3 = new Form3();
+                        f3.tcProfil.Text = txtTCLogin.Text;
+                        this.Hide();
+                        f3.Show();
+                    }  
+                }
+                else
+                {
+                    Temizle();
+                    MessageBox.Show("Bilgileriniz Doğru Değil !!! \nLütfen Kullanıcı Tipini, Tc Kimlik Numaranızı ve Şifrenizi Doğru Girdiğinize Emin Olunuz...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                //komut = new OleDbCommand();
+                //komut.CommandText = "select UserTypeName from UserTypes";
+                //komut.Connection = baglanti;
+
+
+            }
+            
+        }
+
+        private void linkUyeKaydı_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ÜyeOl üye = new ÜyeOl();
+            üye.Show();
+        }
+    }
+}
---
+++ yazilimYapimi/yazilimYapimi/ÜyeOl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace yazilimYapimi
+{
+    public partial class ÜyeOl : Form
+    {
+        public ÜyeOl()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti;
+        OleDbCommand komut;
+        OleDbDataReader oku;
+        
+        private void Temizle()
+        {
+            txtÜyeOlAd.Clear();
+            txtÜyeOlSoyad.Clear();
+            txtÜyeOlTC.Clear();
+            txtÜyeOlTel.Clear();
+            txtÜyeOlAdres.Clear();
+            txtÜyeOlEmail.Clear();
+            txtÜyeOlSifre.Clear();
+            txtÜyeOlSifreTekrar.Clear();
+        }
+        private void btnUyeOl_Click(object sender, EventArgs e)
+        {
+            if (txtÜyeOlSifre.Text==txtÜyeOlSifreTekrar.Text)
+            {
+                    baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Borsa.accdb");
+                    baglanti.Open();
+                    string sqlkodu = "insert into Users([UserTypeName],[UserName],[UserSurname],[User_TC_Number],[TelNumber],[Address],[Email],[Password]) values (@usertypename,@UserName,@UserSurname,@User_TC_Number,@TelNumber,@Address,@Email,@Password)";
+                    komut = new OleDbCommand(sqlkodu, baglanti);
+
+                    komut.Parameters.AddWithValue("@usertypename", "Kullanıcı");
+                    komut.Parameters.AddWithValue("@UserName", txtÜyeOlAd.Text);
+                    komut.Parameters.AddWithValue("@UserSurname", txtÜyeOlSoyad.Text);
+                    komut.Parameters.AddWithValue("@User_TC_Number", Convert.ToInt32(txtÜyeOlTC.Text));
+                    komut.Parameters.AddWithValue("@TelNumber", Convert.ToInt32(txtÜyeOlTel.Text));
+                    komut.Parameters.AddWithValue("@Address", txtÜyeOlAdres.Text);
+                    komut.Parameters.AddWithValue("@Email", txtÜyeOlEmail.Text);
+                    komut.Parameters.AddWithValue("@Password", txtÜyeOlSifre.Text);
+                    oku = komut.ExecuteReader();
+
+                sqlkodu = "insert into UserTypes(UserTypeName)Values(@usertype)";
+                komut = new OleDbCommand(sqlkodu, baglanti);
+                komut.Parameters.AddWithValue("@usertype", "Kullanıcı");
+                
+                baglanti.Close();
+                    Temizle();
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Şifre ve Şifre Tekrar Alanının Aynı Olduğundan Emin Olun", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+            
+        }
+
+        
+    }
+}
